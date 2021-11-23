24c9031f0ffcbd4a45a55c3b7dfa2a2ce97888be
Liewzixian <83172804+Liewzixian@users.noreply.github.com>
2021-11-23 18:10:19 +0800

    Update GUI with Menu

---
+++ demo1/src/main/java/com/example/demo1/MenuController.java
@@ -0,0 +1,41 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button ManageButton;
+
+    @FXML
+    private Button DeleteButton;
+
+    @FXML
+    private Button AfterMenu;
+
+    @FXML
+    private Label MainMenu;
+
+    @FXML
+    protected void addButtonOnAction(){
+    }
+
+    @FXML
+    protected void manageButtonOnAction(){
+    }
+
+    @FXML
+    protected void deleteButtonOnAction(){
+    }
+
+    @FXML
+    protected void afterMenuButtonOnAction(){
+    }
+
+}
